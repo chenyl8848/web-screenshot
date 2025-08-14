a87bb3c28d39d9b2aeb802f2f0578dcf8a2201b3
chenyl8848 <1063415880@qq.com>
2025-08-14 17:17:02 +0800

    init:项目初始化

---
+++ background.js
@@ -0,0 +1,25 @@
+// background.js
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+    if (message.action === 'download') {
+        console.log(message, sender, sendResponse, 'download')
+        try {
+            // 直接使用从内容脚本接收的 base64 数据
+            chrome.downloads.download({
+                // url: `data:image/${message.format};base64,${message.data}`,
+                url: `${message.data}`,
+                filename: `screenshot-${new Date().getTime()}.${message.format}`
+            }, (downloadId) => {
+                if (chrome.runtime.lastError) {
+                    sendResponse({ success: false, error: chrome.runtime.lastError.message });
+                } else {
+                    sendResponse({ success: true, downloadId: downloadId });
+                }
+            });
+
+            // 异步响应标识
+            return true;
+        } catch (error) {
+            sendResponse({ success: false, error: error.message });
+        }
+    }
+});
